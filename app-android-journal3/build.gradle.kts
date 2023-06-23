695d5f2a4406cb504d5c00f1977b2d7fe0fa17be
Hadi Satrio <hi@hadisatrio.com>
2023-06-23 11:01:31 +0700

    android/journal3: Declare signing configs
    Override the default signing keys for debug builds (so application
    updates can happen across different developer machines) and define
    a new config for release builds.

--- app-android-journal3/build.gradle.kts
+++ app-android-journal3/build.gradle.kts
@@ -13,13 +13,34 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        getByName("debug") {
+            storeFile = File(projectDir, "debug_signing.jks")
+            storePassword = "(debug)"
+            keyAlias = "debug"
+            keyPassword = "(debug)"
+        }
+        create("release") {
+            storeFile = File(projectDir, "release_signing.jks")
+            storePassword = System.getenv("RELEASE_KEY_STORE_PASSWORD")
+            keyAlias = System.getenv("RELEASE_KEY_ALIAS")
+            keyPassword = System.getenv("RELEASE_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         debug {
+            signingConfig = signingConfigs.getByName("debug")
             buildConfigField("String", "KEY_HERE_API", "\"${System.getenv("DEBUG_KEY_HERE_API")}\"")
         }
         release {
-            buildConfigField("String", "KEY_HERE_API", "\"${System.getenv("DEBUG_KEY_HERE_API")}\"")
+            signingConfig = signingConfigs.getByName("release")
+            buildConfigField("String", "KEY_HERE_API", "\"${System.getenv("RELEASE_KEY_HERE_API")}\"")
         }
+    }
+
+    packagingOptions {
+        resources.excludes.add("META-INF/*")
     }
 }
 
