aabeff713f078cff59a4dac40b50e7d7501f26df
Hadi Satrio <hi@hadisatrio.com>
2023-11-28 23:19:04 +0700

    root: Remove legacy plugin declarations
    To prevent a similar issue happening again, ensure that we won't keep
    the legacy plugin declarations around.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,6 @@ buildscript {
         mavenCentral()
         maven(url = "https://ajoberstar.org/bintray-backup/")
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-        classpath("androidx.benchmark:benchmark-gradle-plugin:1.2.1")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-        classpath("org.jfrog.buildinfo:build-info-extractor-gradle:5.1.10")
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
-    }
 }
 
 allprojects {
