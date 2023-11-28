ccacf660d1063f3b897803e1fb8984f7f7cde7ac
Hadi Satrio <hi@hadisatrio.com>
2023-11-28 23:08:30 +0700

    root: Remove redundant plugin declaration
    The redundant declaration apparently have shrouded the fact that we
    have not done a proper migration to Kover's new APIs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ buildscript {
         classpath("androidx.benchmark:benchmark-gradle-plugin:1.2.1")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
         classpath("org.jfrog.buildinfo:build-info-extractor-gradle:5.1.10")
-        classpath("org.jetbrains.kotlinx:kover:0.6.1")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
     }
 }
