20b93e6b8f8f06fc71b81ff9e53ab6b35561db8f
Hadi Satrio <hi@hadisatrio.com>
2024-12-17 22:04:59 +0700

    kotlin/geography: Model nearby places backed by Google APIs
    Model a collection of places situated in the vicinity of a given
    coordinate values, backed with data from Google Places APIs.

---
+++ lib-kmm-geography/src/commonMain/kotlin/com/hadisatrio/libs/kotlin/geography/google/GoogleNearbyPlaces.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2022 Hadi Satrio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hadisatrio.libs.kotlin.geography.google
+
+import com.benasher44.uuid.Uuid
+import com.hadisatrio.libs.kotlin.geography.Coordinates
+import com.hadisatrio.libs.kotlin.geography.LiteralCoordinates
+import com.hadisatrio.libs.kotlin.geography.Place
+import com.hadisatrio.libs.kotlin.geography.Places
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.URLBuilder
+import io.ktor.http.appendEncodedPathSegments
+import io.ktor.http.clone
+import io.ktor.http.isSuccess
+import io.ktor.utils.io.errors.IOException
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+
+class GoogleNearbyPlaces(
+    private val coordinates: Coordinates,
+    private val limit: Int,
+    private val apiKey: String,
+    private val httpClient: HttpClient
+) : Places {
+
+    private val urlBuilder: URLBuilder by lazy {
+        URLBuilder("https://places.googleapis.com/v1")
+    }
+
+    private val pastResponses: MutableMap<Coordinates, Set<GooglePlace>> by lazy { mutableMapOf() }
+    private val pastSearchResults: MutableSet<GooglePlace> by lazy { mutableSetOf() }
+
+    override fun new(): Place {
+        throw UnsupportedOperationException("This collection of places is read-only.")
+    }
+
+    override fun findPlace(id: Uuid): Iterable<Place> {
+        // I don't really like this very much. Ideally, we should be able to perform
+        // an ID-based query to Google's services. However, the fact that they are not
+        // using UUIDs prevents us from doing so. On the other hand, we have enforced the
+        // usage of UUIDs through `Uuid#nameUUIDFromBytes()`, which internally uses
+        // MD5, making it nearly impossible to retrieve the original ID.
+        // The best we can do then is a simple filter operation on the data we have
+        // obtained so far from Google, either through `findPlace(String)` or `iterator()`.
+        return pastSearchResults.filter { it.id == id } + this.filter { it.id == id }
+    }
+
+    override fun findPlace(name: String): Iterable<Place> = runBlocking {
+        val urlBuilder = urlBuilder.clone().appendEncodedPathSegments("places:searchText")
+        val request = GooglePlacesRequest(coordinates, DISTANCE_THRESHOLD_METERS, limit, name)
+        val places = jsonPlaces(urlBuilder.buildAndCall(request.toString()).body())
+        pastSearchResults.addAll(places)
+        return@runBlocking places
+    }
+
+    override fun iterator(): Iterator<Place> = runBlocking {
+        val coordinates = LiteralCoordinates(coordinates.toString())
+        return@runBlocking (cachedPlaces(coordinates) ?: httpPlaces(coordinates)).iterator()
+    }
+
+    private fun cachedPlaces(coordinates: Coordinates): Iterable<GooglePlace>? {
+        return pastResponses.entries
+            .firstOrNull { (key, _) -> key.distanceTo(coordinates).value <= DISTANCE_THRESHOLD_METERS }
+            ?.value
+    }
+
+    private suspend fun httpPlaces(coordinates: Coordinates): Iterable<GooglePlace> {
+        val urlBuilder = urlBuilder.clone().appendEncodedPathSegments("places:searchNearby")
+        val request = GooglePlacesRequest(coordinates, DISTANCE_THRESHOLD_METERS, limit)
+        val places = jsonPlaces(urlBuilder.buildAndCall(request.toString()).body())
+        pastResponses[coordinates] = places.toSet()
+        return places
+    }
+
+    private fun jsonPlaces(json: String): Iterable<GooglePlace> {
+        val responseObject = Json.parseToJsonElement(json).jsonObject
+        val responseArray = responseObject["places"]?.jsonArray
+        return responseArray?.map { GooglePlace(it) }.orEmpty()
+    }
+
+    private suspend fun URLBuilder.buildAndCall(body: String): HttpResponse {
+        val url = this.build()
+        val response = httpClient.post(url) {
+            header("X-Goog-Api-Key", apiKey)
+            header("X-Goog-FieldMask", "places.id,places.displayName,places.shortFormattedAddress,places.location")
+            setBody(body)
+        }
+        if (!response.status.isSuccess()) throw IOException("HTTP ${response.status}: ${response.body<String>()}.")
+        return response
+    }
+
+    companion object {
+        private const val DISTANCE_THRESHOLD_METERS = 100
+    }
+}
---
+++ lib-kmm-geography/src/commonMain/kotlin/com/hadisatrio/libs/kotlin/geography/google/GooglePlace.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022 Hadi Satrio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hadisatrio.libs.kotlin.geography.google
+
+import com.benasher44.uuid.Uuid
+import com.hadisatrio.libs.kotlin.geography.Coordinates
+import com.hadisatrio.libs.kotlin.geography.LiteralCoordinates
+import com.hadisatrio.libs.kotlin.geography.Place
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+internal class GooglePlace(
+    private val jsonObject: JsonObject
+) : Place {
+
+    override val id: Uuid by lazy {
+        val googleId = jsonObject["id"]!!.jsonPrimitive.content // "ChIJJUGQ2fHzaS4RVMXJKuiojVE"
+        Uuid.nameUUIDFromBytes(googleId.toByteArray())
+    }
+
+    override val name: String by lazy {
+        val displayName = jsonObject["displayName"]!!.jsonObject
+        displayName["text"]!!.jsonPrimitive.content
+    }
+
+    override val address: String by lazy {
+        jsonObject["shortFormattedAddress"]!!.jsonPrimitive.content
+    }
+
+    override val coordinates: Coordinates by lazy {
+        val position = jsonObject["location"]!!.jsonObject
+        LiteralCoordinates("${position["latitude"]!!},${position["longitude"]!!}")
+    }
+
+    constructor(jsonElement: JsonElement) : this(jsonElement.jsonObject)
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is GooglePlace) return false
+        if (id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
